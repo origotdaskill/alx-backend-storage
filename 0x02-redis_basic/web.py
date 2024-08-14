8deeaeee03e3c574b388824b61f6c4152724087b
origotdaskill <mr.walid.ori@gmaill.com>
2024-08-14 12:57:04 +0100

    ori commit

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 """
-Implements an expiring web cache and tracker
+web cache and tracker
 """
-from typing import Callable
-from functools import wraps
-import redis
 import requests
-redis_client = redis.Redis()
+import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
-def url_count(method: Callable) -> Callable:
-    """counts how many times an url is accessed"""
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(f'{url}')
-        if cached:
-            return cached.decode('utf-8')
-        redis_client.setex(f'{url}, 10, {method(url)}')
-        return method(*args, **kwargs)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@url_count
+@count_url_access
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
